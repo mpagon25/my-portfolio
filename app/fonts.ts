a96260a4098f2801084ff8a1ebeea07706cdf024
Manuel Pages Gonzales <m.pago92@gmail.com>
2025-04-17 13:56:34 +0200

    Setup fonts and remove default global styles

---
+++ app/fonts.ts
@@ -0,0 +1,19 @@
+import { Share, Share_Tech, Share_Tech_Mono } from "next/font/google";
+
+export const share = Share({
+  variable: "--font-share",
+  subsets: ["latin"],
+  weight: "400",
+});
+
+export const shareTech = Share_Tech({
+  variable: "--font-share-tech",
+  subsets: ["latin"],
+  weight: "400",
+});
+
+export const shareTechMono = Share_Tech_Mono({
+  variable: "--font-share-tech-mono",
+  subsets: ["latin"],
+  weight: "400",
+});
